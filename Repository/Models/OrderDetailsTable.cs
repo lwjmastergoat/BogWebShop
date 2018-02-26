2e7e00c556d2c4b6e505e41a86759ac70f271d31
VID\lass4479 <lwjmarketing@outlook.com>
2018-02-26 10:03:58 +0100

    da

---
+++ Repository/Models/OrderDetailsTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Models
+{
+    public class OrderDetailsTable
+    {
+        public int ID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductsID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
