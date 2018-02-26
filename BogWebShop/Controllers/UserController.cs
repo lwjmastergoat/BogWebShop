926c75c13c91a0e9c43d842bd068b39e95f76f69
ChokoBeans <chokobeans95@gmail.com>
2018-02-26 10:52:05 +0100

    jklsdkajlkdsa

---
+++ BogWebShop/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BogWebShop.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string email, string password)
+        {
+
+            return Redirect("/Home/");
+        }
+
+    }
+}
