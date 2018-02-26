75a10ded6978a88a5280566e4758eb18020e8a5e
ChokoBeans <chokobeans95@gmail.com>
2018-02-26 12:08:48 +0100

    dsakældskaældksælsakdælsakældkæalskæ

--- BogWebShop/Controllers/UserController.cs
+++ BogWebShop/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Repository.Factories;
+using Repository.Models;
 
 namespace BogWebShop.Controllers
 {
@@ -16,7 +18,11 @@ namespace BogWebShop.Controllers
         [HttpPost]
         public ActionResult Index(string email, string password)
         {
-
+            UsersFactory uf = new UsersFactory();
+            UsersTable ut = new UsersTable();
+            ut = uf.LogIn(email.Trim(), password.Trim());
+            Session["Rolle"] = ut.Role;
+            
             return Redirect("/Home/");
         }
 
