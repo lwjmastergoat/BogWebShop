0038c8dcbf5f2cd7b69cbe2cc34d6a9691804a28
VID\lass4479 <lwjmarketing@outlook.com>
2018-02-26 09:54:48 +0100

    dasda

---
+++ Repository/Models/CategoryTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Models
+{
+    public class CategoryTable
+    {
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
