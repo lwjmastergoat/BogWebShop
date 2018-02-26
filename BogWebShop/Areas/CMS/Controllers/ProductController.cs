a59c07d1d9b2fd40616198fea6ce7c6dfb6e9773
VID\lass4479 <lwjmarketing@outlook.com>
2018-02-26 12:19:05 +0100

    asdasd

---
+++ BogWebShop/Areas/CMS/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BogWebShop.Areas.CMS.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        int Admin = 2;
+        int Editor = 1;
+        // GET: CMS/Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
