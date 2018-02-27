b78ad0c3e5cf114487b29d879a75415552430ab9
VID\lass4479 <lwjmarketing@outlook.com>
2018-02-27 09:33:09 +0100

    fafafaf

---
+++ BogWebShop/ViewModels/ProductVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Repository.Models;
+
+namespace BogWebShop.ViewModels
+{
+    public class ProductVM
+    {
+        public ProductsTable Products { get; set; }
+
+        public List<CategoryTable> Category { get; set; }
+
+    }
+}
