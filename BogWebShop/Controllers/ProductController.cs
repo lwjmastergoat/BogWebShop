6b9c2608a10ee96dcb9797641b567e1a4a03b236
VID\lass4479 <lwjmarketing@outlook.com>
2018-02-28 09:04:30 +0100

    asdasda

--- BogWebShop/Controllers/ProductController.cs
+++ BogWebShop/Controllers/ProductController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Repository.Factories;
+using BogWebShop.ViewModels;
 
 namespace BogWebShop.Controllers
 {
     public class ProductController : Controller
     {
         ProductFactory pf = new ProductFactory();
+        AuthorFactory af = new AuthorFactory();
         
         // GET: Product
         public ActionResult Index()
@@ -20,7 +22,12 @@ namespace BogWebShop.Controllers
         public ActionResult Details(int ID)
         {
 
-            return View(pf.Get(ID));
+            AuthorVM vm = new AuthorVM();
+
+            vm.Products = pf.Get(ID);
+            vm.Authors = af.Get(vm.Products.AuthorID);
+
+            return View(vm);
         }
         
     }
