b7081ea1e5c7c4e7207914660957901802c59278
VID\lass4479 <lwjmarketing@outlook.com>
2018-02-28 09:27:44 +0100

    ad

--- BogWebShop/Areas/CMS/Controllers/OrderController.cs
+++ BogWebShop/Areas/CMS/Controllers/OrderController.cs
@@ -13,5 +13,10 @@ namespace BogWebShop.Areas.CMS.Controllers
         {
             return View();
         }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
     }
 }
