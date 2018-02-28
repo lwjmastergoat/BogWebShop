79d6a818fd62edb58b28e668d5c265efd54fa43d
MrNissenDK <mrnissen@live.dk>
2018-02-28 13:34:44 +0100

    categorySelcet

---
+++ BogWebShop/JavaScripts/CategorySelcet.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener("DOMContentLoaded", start);
+var categorys;
+
+function start() {
+	var LS = localStorage.getItem("cart");
+	cart = JSON.parse(LS);
+	fetch('/api/APICategory/GetCategorys', {
+		method: 'Get'
+	})
+		.then(Response => Response.json())
+		.then(res => {
+			console.log(res);
+			categorys = res;
+		});
+};
+function MakeSelcet() {
+	for (var i in category) {
+		var category = categorys[i];
+		"<option value=" + category.ID + ">" + category.CategoryName + "</option>";
+	}
+}
